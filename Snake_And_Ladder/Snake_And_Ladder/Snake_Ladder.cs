8fbbb1d4a9b6cfc88de9137ca4fbd9d2f66c97b1
anandsaikumar <anandsai298@gmail.com>
2023-03-06 21:30:11 +0530

    [sai] Add : Winning position 100 and if player possition is lessthan zero the starts gain fom zero position

--- Snake_And_Ladder/Snake_And_Ladder/Snake_Ladder.cs
+++ Snake_And_Ladder/Snake_And_Ladder/Snake_Ladder.cs
@@ -8,29 +8,38 @@ namespace Snake_And_Ladder
 {
     public class Snake_Ladder
     {
-        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2,WINNING_POSITION=100;
         int Player_Position = 0;
         Random random = new Random();
         public int Dice_Role()
         {
             int Die = random.Next(1, 7);
-            Console.WriteLine(Die);
+            Console.WriteLine("die role ---->" + Die);
             return Die;
         }
         public void Game()
         {
-            int option = random.Next(0, 3);
-            switch(option)
-            {
-                case NO_PLAY://no change in position
-                    break;
-                case LADDER://increses player position by dicerole number
-                    this.Player_Position += Dice_Role();
-                    break;
-                case SNAKE://decreases player position by dicerole number
-                    this.Player_Position -= Dice_Role();
-                    break;
+            while(this.Player_Position < WINNING_POSITION)//player position moves unltil 100
+            { 
+                int option = random.Next(0, 3);
+                switch(option)
+                {
+                    case NO_PLAY://no change in position
+                        break;
+                    case LADDER://increses player position by dicerole number
+                        this.Player_Position += Dice_Role();
+                        break;
+                    case SNAKE://decreases player position by dicerole number
+                        int DiceRole = Dice_Role();//if player position is lessthan 0 then starts from position 0
+                        if (this.Player_Position - DiceRole > 0)
+                            this.Player_Position -= DiceRole;
+                        else
+                            this.Player_Position = 0;
+                        break;
+                }
+                Console.WriteLine("player position -------> " + Player_Position);
             }
+            Console.WriteLine("player position -------> " + Player_Position);
         }
     }
 }
